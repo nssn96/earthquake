bf3165de58065cebdf37d4e53951065db73ee191
Your Name <nssurya1996@gmail.com>
2022-02-19 05:37:56 -0600

    added the lat-long identify part

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 # Name : Surya Narayanan Nadhamuni Suresh
 # UTA ID : 1001877873
 
+from distutils.command.build_scripts import first_line_re
 from flask import Flask, render_template, request,url_for,flash
 import mysql.connector as mysql
+from geopy.geocoders import Nominatim as nm
+from geopy.point import Point
+
 
 app = Flask(__name__)
 app.secret_key = 'random string'
@@ -56,7 +60,6 @@ def dateRange(fields):
     conn.close()
     return res
 
-
 def groupByMag(fields):
     query = "select t.new as 'mag_range', count(*) as 'number_of_occurences' from ( "
     query+="select case when mag>=1 and mag<2 then '1-2' "
@@ -76,8 +79,28 @@ def groupByMag(fields):
     conn.close()
     return res
 
+#References used for the below function
+#https://www.tutorialspoint.com/how-to-get-the-longitude-and-latitude-of-a-city-using-python
+def getLatLong(place):
+    #initialize the Nominatim API
+    locator = nm(user_agent="MyApp")
+
+    location = locator.geocode(place)
+    return location
+
+#https://stackoverflow.com/questions/57770044/valueerror-must-be-a-coordinate-pair-or-point
+def getPlaceName(fields):
+    #initialize the Nominatim API
+    locator = nm(user_agent="MyApp")
+    co_ord = '"'+fields['lat']+' , '+fields['long']+'"'
+    location = locator.reverse(Point(fields['lat'],fields['long']))
+    address = location.raw['address']
+    #address is a dictionary with all the details of the lat,long
+    #print(address)
+    return address
     
-        
+
+
 
 
 
@@ -85,6 +108,47 @@ def groupByMag(fields):
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/placename',methods=['GET','POST'])
+def placeName():
+    if request.method=='POST':
+        dic={}
+        for key,value in request.form.items():
+            if value!='':
+                dic[key]=value
+        
+        #print(dic)
+
+        if dic:
+            result=getPlaceName(dic)
+        else:
+            result=[]
+            flash('Please enter values in the field')
+    
+    return render_template('index.html',place=result)
+
+
+
+@app.route('/latlong',methods=['GET','POST'])
+def latLong():
+    if request.method=='POST':
+        dic={}
+        for key,value in request.form.items():
+            if value!='':
+                dic[key]=value
+        
+        #print(dic)
+
+        if dic:
+            result=getLatLong(dic['location'])
+            print(result.address)
+        else:
+            result=[]
+            flash('Please enter values in the field')
+    
+    return render_template('index.html', loc=result)
+
+
 
 @app.route('/search',methods=['GET','POST'])
 def search():
